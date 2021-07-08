18f631de6f8217c45fec4fa450e59a68bdb132f3
giglioq <quinten.g.giglio@lawrence.edu>
2021-07-08 12:49:45 -0500

    Summer 2021 check up

--- qg/XRD_methods.py
+++ qg/XRD_methods.py
@@ -14,6 +14,7 @@ from lmfit import models
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
